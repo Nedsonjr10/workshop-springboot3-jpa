a7105dd032c0b087337a390ac4c11af888c9e06d
Nedson Nogueira <nedson.junior91@gmail.com>
2023-11-12 20:19:25 -0300

    Payment, one-to-one association

--- src/main/java/com/educandoweb/course/config/testConfig.java
+++ src/main/java/com/educandoweb/course/config/testConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.educandoweb.course.entities.Category;
 import com.educandoweb.course.entities.Order;
+import com.educandoweb.course.entities.Payment;
 import com.educandoweb.course.entities.Product;
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.entities.enums.OrderStatus;
@@ -79,5 +80,9 @@ public class testConfig implements CommandLineRunner {
 	
 		orderitemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 	
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"),o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
